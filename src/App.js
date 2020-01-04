e309d6ee8e9dfe4ed22adac4c553d8bc43a94351
chuk <chuk@localhost.localdomain>
2020-01-03 21:49:29 -0600

    First test toward building fee function

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
-export const fee = () => {};
+export const fee = start => {
+  var checkStartPm = start.search("pm");
+
+  var startTime = timeConvert(start);
+
+  if (checkStartPm > 0 && startTime < 17) {
+    return ["Cannot start before 5:00pm", "Mistake made with entry"];
+  } else if (checkStartPm < 0 && startTime > 4) {
+    return ["Cannot start before 5:00pm", "Mistake made with entry"];
+  }
+};
+
+export const timeConvert = start => {
+  start = start.replace(":", ".");
+  var startInt = parseInt(start);
+  var checkStartPm = start.search("pm");
+  if (checkStartPm > 0 && startInt !== 12) {
+    var startFloat = parseFloat(start);
+    var startTime = startFloat + 12;
+  } else if (checkStartPm < 0 && startInt == 12) {
+    var startFloat = parseFloat(start);
+    var startTime = startFloat - 12;
+  } else {
+    var startTime = parseFloat(start);
+  }
+
+  return startTime;
+};
--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,26 @@
-import { fee } from "./App";
+import { fee, timeConvert } from "./App";
+
+describe("timeConvert", () => {
+  it("should convert start time and end time to a number", () => {
+    var start = "5:15pm";
+    expect(typeof timeConvert(start)).toEqual("number");
+  });
+  it("start time and end time should be less than 24 hours", () => {
+    var start = "5:15pm";
+    expect(timeConvert(start)).toBeLessThan(24);
+  });
+  it("start time and end time should be more than 0 hours", () => {
+    var start = "5:15pm";
+
+    expect(timeConvert(start)).toBeGreaterThanOrEqual(0);
+  });
+});
+
+describe("fee", () => {
+  it("starts no earlier than 5:00pm", () => {
+    var family = "Family A";
+    var start = "12:15pm";
+    var end = "3:10am";
+    expect(fee(start, end, family)).toContain("Cannot start before 5:00pm");
+  });
+});
