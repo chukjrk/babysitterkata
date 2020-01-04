b9c1c44e818edec90ef41d65f35c028d2d7394de
chuk <chuk@localhost.localdomain>
2020-01-03 22:11:39 -0600

    Next test for leave time requirment

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-export const fee = start => {
+export const fee = (start, end) => {
   var checkStartPm = start.search("pm");
+  var checkEndPm = end.search("am");
 
-  var startTime = timeConvert(start);
+  var { startTime, endTime } = timeConvert(start, end);
 
   if (checkStartPm > 0 && startTime < 17) {
     return ["Cannot start before 5:00pm", "Mistake made with entry"];
   } else if (checkStartPm < 0 && startTime > 4) {
     return ["Cannot start before 5:00pm", "Mistake made with entry"];
   }
+  if (checkEndPm > 0 && endTime > 4) {
+    return ["Cannot leave later than 4:00am", "Mistake made with entry"];
+  } else if (checkEndPm < 0 && endTime < 17) {
+    return ["Cannot leave later than 4:00am", "Mistake made with entry"];
+  }
 };
 
-export const timeConvert = start => {
+export const timeConvert = (start, end) => {
   start = start.replace(":", ".");
+  end = end.replace(":", ".");
+  var endInt = parseInt(end);
   var startInt = parseInt(start);
   var checkStartPm = start.search("pm");
+  var checkEndPm = end.search("am");
   if (checkStartPm > 0 && startInt !== 12) {
     var startFloat = parseFloat(start);
     var startTime = startFloat + 12;
@@ -23,6 +32,14 @@ export const timeConvert = start => {
   } else {
     var startTime = parseFloat(start);
   }
-
-  return startTime;
+  if (checkEndPm > 0 && endInt == 12) {
+    var endFloat = parseFloat(end);
+    var endTime = endFloat - 12;
+  } else if (checkEndPm < 0 && endInt !== 12) {
+    var endFloat = parseFloat(end);
+    var endTime = endFloat + 12;
+  } else {
+    var endTime = parseFloat(end);
+  }
+  return { startTime, endTime };
 };
--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,35 @@
 import { fee, timeConvert } from "./App";
 
 describe("timeConvert", () => {
-  it("should convert start time and end time to a number", () => {
+  it("should convert start time to a number", () => {
     var start = "5:15pm";
-    expect(typeof timeConvert(start)).toEqual("number");
+    var end = "3:10am";
+    expect(typeof timeConvert(start, end).startTime).toEqual("number");
   });
-  it("start time and end time should be less than 24 hours", () => {
+  it("start time should be less than 24 hours", () => {
     var start = "5:15pm";
-    expect(timeConvert(start)).toBeLessThan(24);
+    var end = "3:10am";
+    expect(timeConvert(start, end).startTime).toBeLessThan(24);
   });
-  it("start time and end time should be more than 0 hours", () => {
+  it("start time should be more than 0 hours", () => {
     var start = "5:15pm";
-
-    expect(timeConvert(start)).toBeGreaterThanOrEqual(0);
+    var end = "3:10am";
+    expect(timeConvert(start, end).startTime).toBeGreaterThanOrEqual(0);
+  });
+  it("should convert end time to a number", () => {
+    var start = "5:15pm";
+    var end = "3:10am";
+    expect(typeof timeConvert(start, end).endTime).toEqual("number");
+  });
+  it("end time should be less than 24 hours", () => {
+    var start = "5:15pm";
+    var end = "3:10am";
+    expect(timeConvert(start, end).endTime).toBeLessThan(24);
+  });
+  it("end time should be more than 0 hours", () => {
+    var start = "5:15pm";
+    var end = "3:10am";
+    expect(timeConvert(start, end).endTime).toBeGreaterThanOrEqual(0);
   });
 });
 
@@ -22,5 +39,11 @@ describe("fee", () => {
     var start = "12:15pm";
     var end = "3:10am";
     expect(fee(start, end, family)).toContain("Cannot start before 5:00pm");
+  });
+  it("should leave no later than 4:00am", () => {
+    var family = "Family A";
+    var start = "5:15pm";
+    var end = "12:10pm";
+    expect(fee(start, end, family)).toContain("Cannot leave later than 4:00am");
   });
 });
